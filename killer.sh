3a83dc67f2369bb94ef8bc59edd72218180e7edd
U-L-M-S <96896184+U-L-M-S@users.noreply.github.com>
2023-02-12 20:39:30 +0100

    Add a line of information in the end
    Now the user can see how many files were moved to the container

--- killer.sh
+++ killer.sh
@@ -5,33 +5,32 @@ total_files=$(find . -maxdepth 1 -name "*.png" | wc -l)
 counter=0
 
 if [ ! -d "$container" ]; then
-  mkdir "$container"
+mkdir "$container"
 fi
 
 for file in *.png
 do
-    # Get all the means
-    means=($(identify -verbose "$file" | awk '/mean:/ {print $2}'))
-    mean=0
-    # Iterate through all the means
-    for i in "${means[@]}"
-    do
-        mean=$(echo "($mean + $i)/${#means[@]}" | bc -l)
-    done
-    if (( $(echo "$mean > 80" | bc -l) )); then
-        mv "$file" "$container"
-        continue
-    fi
+# Get all the means
+means=($(identify -verbose "$file" | awk '/mean:/ {print $2}'))
+mean=0
+# Iterate through all the means
+for i in "${means[@]}"
+do
+mean=$(echo "($mean + $i)/${#means[@]}" | bc -l)
+done
+if (( $(echo "$mean > 80" | bc -l) )); then
+mv "$file" "$container"
+continue
+fi
+white_percent=$(convert "$file" -format "%[fx:mean*100]" info:)
+# Check if the percentage of white color is greater than 40%
+if [ $(echo "$white_percent > 40" | bc) -eq 1 ]; then
+  echo "Moving $file to container"
+  mv "$file" "$container"
+fi
+counter=$((counter+1))
+echo -ne "Processing $file... $(echo "scale=2; $counter*100/$total_files" | bc)%\r"
 
-    white_percent=$(convert "$file" -format "%[fx:mean*100]" info:)
-    # Check if the percentage of white color is greater than 25%
-    if [ $(echo "$white_percent > 25" | bc) -eq 1 ]; then
-      echo "Moving $file to container directory"
-      mv "$file" "$container"
-    fi
-    counter=$((counter+1))
-    echo -ne "Processing $file... $(echo "scale=2; $counter*100/$total_files" | bc)%\r"
 done
 
-echo "Done"
-
+echo "Done. $counter of $total_files images moved into the container"
